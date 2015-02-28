62cac5251a566c9a911b1e35ca427ae9bb616d74
Thomas H <thimblot@gmail.com>
2015-02-28 18:06:27 +0100

    deleting dead code

--- src/main/java/com/tomahim/jsonUtils/builders/JsonTreeBuilder.java
+++ src/main/java/com/tomahim/jsonUtils/builders/JsonTreeBuilder.java
@@ -9,44 +9,6 @@ public class JsonTreeBuilder {
 	
 	final static String DOT = ".";
 	final static String DOT_SPLIT_REGEX = "\\."; 
-
-
-	public static void main(String[] args) {
-		
-		JsonNode country = new JsonNode();	
-		JsonNode countryName = new JsonNode("name", "name");
-		country.addNode(countryName);
-
-		Map<String, String> selection  = new HashMap<String, String>();
-		selection.put("id", "id");
-		selection.put("name", "name");
-		selection.put("region.id", "region.id");
-		selection.put("region.name", "region.name");
-		selection.put("cities.name", "cities.name");
-		selection.put("cities.id", "cities.id");
-		selection.put("cities.id", "cities.id2");
-		selection.put("region.countries.cities.name", "cities.name2");
-		JsonNode jsonTree = new JsonNode(); 
-
-		constructTreeFromMap(jsonTree, selection);
-		
-		System.out.println("jsonTree " + jsonTree);
-		
-	}
-	
-	/*public static JsonNode findNode(JsonNode node, String keyPath) {
-		if(keyPath.contains(".")) {
-			String[] keys = keyPath.split(DOT_SPLIT_REGEX);
-			JsonNode found = node.findNode(keys[0]);
-			if(found == null) {
-				return node;
-			} else {
-				return node.findNode(getNextValue(keyPath));
-			}
-		} else {
-			return node.findNode(keyPath);
-		}
-	}*/
 	
 	public static JsonNode constructTreeFromMap(JsonNode node, Map<String, String> selection) {
 		for(Map.Entry<String, String> entry : selection.entrySet()) {
