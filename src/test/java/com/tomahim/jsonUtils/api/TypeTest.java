66dbb2c0cf16cee81903265d1ac1155be593cc09
Thomas H <thimblot@gmail.com>
2015-02-28 13:20:03 +0100

    jUnit tests of type conservation through json conversion

--- src/test/java/com/tomahim/jsonUtils/api/TypeTest.java
+++ src/test/java/com/tomahim/jsonUtils/api/TypeTest.java
@@ -8,8 +8,10 @@ import java.util.Date;
 import javax.json.JsonObject;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import com.tomahim.jsonUtils.configuration.JsonUtilsSettings;
 import com.tomahim.jsonUtils.entities.AllTypes;
 
 public class TypeTest {
@@ -39,8 +41,14 @@ public class TypeTest {
 	
 	public JsonObject jsonObject;
 	
+	public static String[] keys = {"typeBoolean", "typeCharacter", "typeByte", "typeDate", "typeShort", 
+			"typeInteger", "typeLong", "typeFloat", "typeDouble", "typeBigDecimal", 
+			"typeString", "primitiveInt", "primitiveBoolean", "primitiveByte", 
+			"primitiveLong", "primitiveFloat", "primitiveDouble", "primitiveChar"};
+	
 	@Before
 	public void init() {
+		JsonUtilsSettings.resetSettings();
 		types = new AllTypes(typeBoolean, typeCharacter, typeByte, typeDate, typeShort, typeInteger, typeLong, 
 							typeFloat, typeDouble, typeBigDecimal, typeString, primitiveInt, primitiveBoolean, 
 							primitiveByte, primitiveLong, primitiveFloat, primitiveDouble, primitiveChar);
@@ -50,14 +58,75 @@ public class TypeTest {
 	@Test
 	public void notNull() {
 		assertNotNull(jsonObject);
-		String[] keys = {"typeBoolean", "typeCharacter", "typeByte", "typeDate", "typeShort", 
-				"typeInteger", "typeLong", "typeFloat", "typeDouble", "typeBigDecimal", 
-				"typeString", "primitiveInt", "primitiveBoolean", "primitiveByte", 
-				"primitiveLong", "primitiveFloat", "primitiveDouble", "primitiveChar"};
 		for(String key : keys) {
 			assertTrue(key + " not supported", jsonObject.containsKey(key));
 		}
 	}
 	
 	//TODO : one method per type checking values of each
+	
+	@Test
+	public void checkBooleanType() {
+		assertEquals(typeBoolean, jsonObject.getBoolean("typeBoolean"));
+		assertEquals(primitiveBoolean, jsonObject.getBoolean("primitiveBoolean"));
+	}
+	
+	@Test
+	@Ignore
+	public void checkCharType() {
+		assertEquals(typeCharacter, jsonObject.getString("typeCharacter"));
+		assertEquals(primitiveChar, jsonObject.getString("primitiveChar"));
+	}
+
+	@Test
+	@Ignore
+	public void checkByteType() {
+		assertEquals(typeByte, jsonObject.getString("typeByte"));
+		assertEquals(primitiveByte, jsonObject.getString("primitiveByte"));
+	}
+	
+	@Test
+	public void checkDateType() {
+		//We expect for date as default to get timestamp value (according to settings default)
+		assertEquals(typeDate.getTime(), jsonObject.getJsonNumber("typeDate").longValue());
+	}	
+	
+	@Test
+	public void checkShortType() {
+		assertTrue(typeShort == jsonObject.getJsonNumber("typeShort").intValueExact());
+	}
+	
+	@Test
+	public void checkDoubleType() {
+		assertTrue(typeDouble == jsonObject.getJsonNumber("typeDouble").doubleValue());
+		assertTrue(primitiveDouble == jsonObject.getJsonNumber("primitiveDouble").doubleValue());
+	}
+	
+	@Test 
+	public void checkIntegerType() {
+		assertTrue(typeInteger == jsonObject.getInt("typeInteger"));
+		assertTrue(primitiveInt == jsonObject.getInt("primitiveInt"));
+	}
+	
+	@Test
+	public void checkLongType() {
+		assertTrue(typeLong == jsonObject.getJsonNumber("typeLong").longValue());
+		assertTrue(primitiveLong == jsonObject.getJsonNumber("primitiveLong").longValue());
+	}
+	
+	@Test
+	public void checkStringType() {
+		assertEquals(typeString, jsonObject.getString("typeString"));
+	}
+	
+	@Test
+	@Ignore
+	public void checkFloatType() {
+		//assertTrue(typeFloat == jsonObject.getJsonNumber("typeFloat").);
+	}
+	
+	@Test
+	public void checkBigDecimalType() {
+		assertEquals(typeBigDecimal, jsonObject.getJsonNumber("typeBigDecimal").bigDecimalValue());
+	}
 }
