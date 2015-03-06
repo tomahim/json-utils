8ab5b8f6d1f2f1153a1d8cda85a50095557f7c92
Thomas H <thimblot@gmail.com>
2015-03-06 20:43:42 +0100

    configure sonar analysis

--- src/main/java/com/tomahim/jsonUtils/api/JsonUtils.java
+++ src/main/java/com/tomahim/jsonUtils/api/JsonUtils.java
@@ -68,7 +68,6 @@ public class JsonUtils {
 			return toJsonFromMap(o, selection);
 		} catch (IllegalAccessException | IllegalArgumentException
 				| InvocationTargetException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
